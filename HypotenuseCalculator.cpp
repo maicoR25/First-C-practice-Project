09619da240bf9e6fc3333b36b5f3555d59572d4e
Michael Rangel <michael.rangel0709@gmail.com>
2026-03-04 01:28:43 -0700

    Finished this quick and simple project lol

--- HypotenuseCalculator.cpp
+++ HypotenuseCalculator.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-    int triangleSide1;
-    int triangleSide2;
-    double hypotenuese;
-    std::cout << "Hello World!";    
+    double triangleSide1;
+    double triangleSide2;
+    double hypotenuse;
+
+    std::cout << "Please enter the first side: ";    
+    std::cin >> triangleSide1;
+    std::cout << "Please enter the second side: ";    
+    std::cin >> triangleSide2;
+
+    hypotenuse = (sqrt(pow(triangleSide1, 2) + pow(triangleSide2, 2)));
+    
+    std::cout << "The hypootenuse is: " << hypotenuse;
+
+    return 0;
 }
